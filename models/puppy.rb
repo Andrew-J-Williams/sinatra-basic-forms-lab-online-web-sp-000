20aaf86865d3151454ad795de7bea247db3a9cbe
Andrew Williams <1williamsaj@gmail.com>
2020-01-19 03:52:06 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -4,8 +4,8 @@ class Puppy
   attr_accessor :name, :breed, :age
 
   def initialize(name, breed, age)
-    @name = name,
-    @breed = breed,
+    @name = name
+    @breed = breed
     @age = age
   end
 
